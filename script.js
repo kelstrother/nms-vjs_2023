6e330fc62a55912dd5e9b0264c1d8566d4901588
Kel Strother <kelstrother@gmail.com>
2023-05-01 14:58:21 -0600

    created functionality to build and address, add it to storage, and display all the built addresses.

--- script.js
+++ script.js
@@ -1,44 +1,13 @@
 const ogGlyphsContainer = document.querySelector(".og-glyphs-container");
 const row1 = document.querySelector(".row-1");
 const row2 = document.querySelector(".row-2");
-const images = document.querySelectorAll("img");
-const addressContainer = document.querySelector(".selected-glyphs-container");
-const addBtn = document.querySelector('.add-btn');
-
-// const originalGlyphs = [
-//   "./assets/the-star-above-water.png",
-//   "./assets/the-hunter.png",
-//   "./assets/the-reflection.png",
-//   "./assets/the-ancient-giant.png",
-//   "./assets/the-obscured-companion.png",
-//   "./assets/the-ascending-orb.png",
-//   "./assets/the-sailor.png",
-//   "./assets/the-lowly-insect.png",
-//   "./assets/the-dragonfly.png",
-//   "./assets/the-spiral-of-reality.png",
-//   "./assets/the-anomaly.png",
-//   "./assets/the-ocean-king.png",
-//   "./assets/the-hut.png",
-//   "./assets/the-vessel-to-beyond.png",
-//   "./assets/the-path-of-the-tree.png",
-//   "./assets/the-universe-triangle.png",
-// ];
-// originalGlyphs.forEach((glyph, index) => {
-//   let fileName = `${glyph}`;
-//   let nameExtract = fileName.slice(9, -4);
-//   let alt = nameExtract.replace(/-/g, " ");
-//   let glyphText = "glyph";
-//   let finalAlt = alt.concat(" ", glyphText);
-
-//   const div = document.createElement("div");
-//   div.classList.add("glyph");
-//   div.innerHTML = `<img class='glyph-img' src='${glyph}' alt='${finalAlt}'>`;
-//   if (index <= 7) {
-//     row1.appendChild(div);
-//   } else {
-//     row2.appendChild(div);
-//   }
-// });
+// const images = document.querySelectorAll("img");
+const chosenGlyphContainer = document.querySelector(".chosen-glyphs-container");
+const addBtn = document.querySelector(".add-btn");
+const storedAddresses = document.querySelector(".stored-addresses");
+const displayBtn = document.querySelector('.display-btn');
+const modal = document.querySelector('.modal');
+const okBtn = document.querySelector('.ok');
 
 const glyphObj = [
   { name: "The Star Above Water", src: "./assets/the-star-above-water.png" },
@@ -61,7 +30,10 @@ const glyphObj = [
   { name: "The Path Of The Tree", src: "./assets/the-path-of-the-tree.png" },
   { name: "The Universe Triangle", src: "./assets/the-universe-triangle.png" },
 ];
-const ogGlyphs = Object.entries(glyphObj);
+
+//~ /////////////////////////////////////
+//!         create imgages from original object          \\
+//~ /////////////////////////////////////
 glyphObj.forEach((glyph, index) => {
   const div = document.createElement("div");
   div.innerHTML = `<img class='glyph-img' src='${glyph.src}' alt='${glyph.name} Glyph'>`;
@@ -72,44 +44,101 @@ glyphObj.forEach((glyph, index) => {
   }
 });
 
+const portalGlyphs = document.querySelectorAll(".glyph-img");
+function chooseGlyph(e) {
+  const clickedGlyph = e.target;
+  const glyphSrc = e.target.src;
+  if (portalAddress.length < 12) {
+    const newGlyph = new Image();
+    newGlyph.src = glyphSrc;
+    newGlyph.classList.add("selected");
+    chosenGlyphContainer.appendChild(newGlyph);
+    portalAddress.push({ src: clickedGlyph.src, alt: clickedGlyph.alt });
+  } else {
+    clickedGlyph.style.cursor = "not-allowed";
+    chosenGlyphContainer.style.setProperty("--none", "block");
+  }
+  return portalAddress;
+}
 
-const glyphs = document.querySelectorAll(".glyph-img");
+let portalAddress = [];
 
-let selectedGlyphs = [];
+function closeModal() {
 
-glyphs.forEach((glyph, index) => {
-  glyph.addEventListener("click", (e) => {
-    const clickedGlyph = e.target;
-    const glyphSrc = e.target.src;
-    if (selectedGlyphs.length !== 12) {
-      selectedGlyphs.push(clickedGlyph);
-      const portalAddress = document.createElement("div");
-      portalAddress.classList.add("selected-row");
-      portalAddress.innerHTML = `<img class='selected' src='${glyphSrc}'>`;
-      addressContainer.appendChild(portalAddress);
-    } else {
-      clickedGlyph.style.cursor = "not-allowed";
-      addressContainer.style.setProperty('--none', 'block')
+}
+//~ /////////////////////////////////////
+//!         add to storage          \\
+//~ /////////////////////////////////////
+function storePortalAddress(e) {
+  e.preventDefault();
+
+  const storedDestinations = getStoredDestinations();
+  if (portalAddress.length === 12) {
+    storedDestinations.push(portalAddress);
+    localStorage.setItem(
+      "exploredDestinations",
+      JSON.stringify(storedDestinations)
+    );
+
+    clearUI();
+    modal.style.display = 'flex';
+  } else {
+    alert("Portal address needs 12 glyphs.");
+  }
+  return storedDestinations;
+}
+
+//~ /////////////////////////////////////
+//!         get from storage          \\
+//~ /////////////////////////////////////
+function getStoredDestinations() {
+  let portalAddress;
+  if (localStorage.getItem("exploredDestinations") === null) {
+    portalAddress = [];
+  } else {
+    portalAddress = JSON.parse(localStorage.getItem("exploredDestinations"));
+  }
+  return portalAddress;
+}
+//~ /////////////////////////////////////
+//!         get from storage add to DOM          \\
+//~ /////////////////////////////////////
+function displayExploredDestinations() {
+  const storedDestinations = getStoredDestinations();
+  storedDestinations.forEach((destination, index) => {
+    const exploredDiv = document.createElement("div");
+    exploredDiv.classList.add("explored-destination");
+    for (let i = 0; i < destination.length; i++) {
+      const exploredPortalAddress = new Image();
+      exploredPortalAddress.classList.add("explored-img");
+      exploredPortalAddress.src = destination[i].src;
+      exploredPortalAddress.alt = destination[i].alt;
+      exploredDiv.appendChild(exploredPortalAddress);
+    }
+    if (index < 10) {
+      storedAddresses.appendChild(exploredDiv);
+      storedAddresses.style.display = 'flex';
+      document.querySelector('.stored-h2').style.display = 'block';
     }
   });
-});
-
-function storePortalAddress(address) {
-  const portalAddresses = getPortalAddresses();
-
-  portalAddresses.push(address);
-
-  localStorage.setItem('addresses', JSON.stringify(portalAddresses))
 }
-
-function getPortalAddresses() {
-  selectedGlyphs = portalAddresses;
-  if(localStorage.getItem('addresses') === null) {
-    portalAddresses = [];
-  } else {
-    portalAddresses = JSON.parse(localStorage.getItem('addresses'));
+//~ /////////////////////////////////////
+//!         CLEARING FIELDS         \\
+//~ /////////////////////////////////////
+function clearUI() {
+  if (storePortalAddress) {
+    const selected = document.querySelectorAll(".selected");
+    selected.forEach((img) => img.remove());
+    chosenGlyphContainer.style.setProperty("--none", "none");
   }
-  return portalAddresses;
 }
 
-addBtn.addEventListener('click', storePortalAddress)
+//~ /////////////////////////////////////
+//!         EVENT LISTENERS         \\
+//~ /////////////////////////////////////
+
+portalGlyphs.forEach((glyph) => glyph.addEventListener("click", chooseGlyph));
+addBtn.addEventListener("click", storePortalAddress);
+displayBtn.addEventListener('click', displayExploredDestinations)
+okBtn.addEventListener('click', () => modal.style.display = 'none')
+// window.addEventListener("DOMContentLoaded", displayExploredDestinations);
